452b28570d59e8cb548285a518c8d967eda6d043
hongquan32004 <117972211+hongquan32004@users.noreply.github.com>
2024-03-31 21:08:37 +0700

    Update day4

--- HomeWork/src/components/Finish/Finish.jsx
+++
@@ -1,9 +0,0 @@
-import './Finish.scss'
-const Finish = () => {
-    return (
-        <div className="footer-page">
-            Trang web được thiết kế bởi trong <span>freepik.com</span>
-        </div>
-    )
-}
-export default Finish;
